7cd8c91ddb8037d3e6d35bd8d34cafe9b598c5d1
Julia Zuin <zuinjulia@hotmail.com>
2024-10-01 23:35:47 -0300

    feat: first commit

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return{"message": "user 1"}
-    
+
+
 class User(Resource):
     def get(self):
         return{"message": "test"}
+
 
 api.add_resource(Users, "/users")
 api.add_resource(User, "/user")
