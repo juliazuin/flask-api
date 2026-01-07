0426229e8f2990dfe79babb9353358615e4408aa
Julia Zuin <zuinjulia@hotmail.com>
2026-01-07 17:44:00 -0300

    feat: update dev config with the right env names

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class LocalConfig:
-    mongo_user = os.getenv("MONGO_USER")
-    mongo_pass = os.getenv("MONGO_PASSWORD")
-    mongo_host = os.getenv("MONGO_HOST")
-    mongo_db = os.getenv("MONGO_DB")
+    mongo_user = os.getenv("MONGODB_USER")
+    mongo_pass = os.getenv("MONGODB_PASSWORD")
+    mongo_host = os.getenv("MONGODB_HOST")
+    mongo_db = os.getenv("MONGODB_DB")
 
     MONGO_URI = (
         f"mongodb://{mongo_user}:{mongo_pass}@{mongo_host}/{mongo_db}?authSource=admin"
@@ -13,8 +13,13 @@ class LocalConfig:
 
 
 class ProdConfig:
+    mongo_user = os.getenv("MONGODB_USER")
+    mongo_pass = os.getenv("MONGODB_PASSWORD")
+    mongo_host = os.getenv("MONGODB_HOST")
+    mongo_db = "users"
+    
     MONGODB_SETTINGS = {
-        "MONGO_URI": "mongodb://admin:admin@mongodb/usuarios?authSource=admin"
+        "MONGO_URI": "mongodb+srv://admin:<db_password>@cluster0.er82gea.mongodb.net/users?appName=Cluster0"
     }
 
 
