9a8862891d9aaaddfb15bd4427dc09fbfab4a5ce
Julia Zuin <zuinjulia@hotmail.com>
2026-01-29 22:29:36 -0300

    feat: update config using mongodb string connection

--- config.py
+++ config.py
@@ -1,26 +1,18 @@
 import os
 
+MONGODB_USER = os.getenv("MONGODB_USER")
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+MONGODB_HOST = os.getenv("MONGODB_HOST")
+MONGODB_DB = os.getenv("MONGODB_DB")
+MONGO_URI = f"mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}/{MONGODB_DB}?authSource=admin"
+
 
 class LocalConfig:
-    MONGODB_USER = os.getenv("MONGODB_USER")
-    MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
-    MONGODB_HOST = os.getenv("MONGODB_HOST")
-    MONGODB_DB = os.getenv("MONGODB_DB")
-
-    MONGO_URI = (
-        f"mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}/{MONGODB_DB}?authSource=admin"
-    )
+    MONGO_URI = MONGO_URI
 
 
 class ProdConfig:
-    MONGODB_USER = os.getenv("MONGODB_USER")
-    MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
-    MONGODB_HOST = os.getenv("MONGODB_HOST")
-    MONGODB_DB = "users"
-
-    MONGODB_SETTINGS = {
-        "MONGO_URI": "mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}/{MONGODB_DB}?appName=Cluster0"
-    }
+    MONGO_URI = MONGO_URI
 
 
 class TestConfig:
